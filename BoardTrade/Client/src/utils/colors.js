3d53d187f41f581dd0401d48b17f7afe87a02d38
Mateusz Wozniak <mat.wozniak@poczta.pl>
2019-01-09 07:23:37 +0100

    Update colors.js

--- BoardTrade/Client/src/utils/colors.js
+++ BoardTrade/Client/src/utils/colors.js
@@ -1,17 +1,30 @@
+//https://www.color-hex.com/
 const colors = {
   white: '#fff',
 
   blue: '#3E76A8',
-  blueLight: '#6093C1',
-  blueLightest: '#90B8DC',
-  blueDark: '#25639A',
-  blueDarkest: '#124A7D',
+  blueLight: '#6491b9',
+  blueLighter: '#8bacca',
+  blueLightest: '#b1c8dc',
+  blueDark: '#315e86',
+  blueDarker: '#254664',
+  blueDarkest: '#182f43',
+  
+  green: '#c2e5ce',
+  greenLight: '#d4ecdc',
+  greenLighter: '#e6f4eb',
+  greenLightest: '#f2f9f5',
+  greenDark: '#aeceb9',
+  greenDarker: '#87a090',
+  greenDarkest: '#4d5b52',
 
   grey: '#d4dde9',
-  greyLight: '#edf1f5',
-  greyLightest: '#f5f7fa',
-  greyDark: '#617691',
-  greyDarkest: '#11253e'
+  greyLight: '#e0e7ef',
+  greyLighter: '#edf1f6',
+  greyLightest: '#f6f8fa',
+  greyDark: '#bec6d1',
+  greyDarker: '#a9b0ba',
+  greyDarkest: '#7f848b'
 }
 
 export default colors
